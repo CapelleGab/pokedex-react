5295ec46897e3a5ef9748c5b80f790ab7fcd03c0
zeloxlive <zeloxlive@gmail.com>
2024-11-06 23:19:02 +0100

    👌 IMPROVE: French version added 1/2

--- src/pages/api/pokemon.js
+++ src/pages/api/pokemon.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PokemonCard from '@/components/cards';
+import {StatsTranslation, TypesTranslation, AbilitiesTranslation} from '@/pages/api/translation'
 
 export default function GetPokemonList({ onPokemonClick }) {
   const [pokemons, setPokemons] = useState([]);
@@ -16,16 +17,45 @@ export default function GetPokemonList({ onPokemonClick }) {
         return response.json();
       })
       .then(data => {
-        const pokemonsData = data.results.map(pokemon => ({
-          name: pokemon.name,
-          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split("/")[6]}.png`
-        }));
-        setPokemons(pokemonsData);
+        const pokemonsData = [];
+
+        data.results.forEach(pokemon => {
+          fetch(pokemon.url)
+            .then(res => {
+              if (!res.ok) {
+                throw new Error("Erreur de requête des détails !");
+              }
+              return res.json();
+            })
+            .then(details => {
+              return fetch(details.species.url)
+                .then(speciesRes => {
+                  if (!speciesRes.ok) {
+                    throw new Error("Erreur de requête des species !");
+                  }
+                  return speciesRes.json();
+                })
+                .then(speciesData => {
+                  const nameInFrench = speciesData.names.find(name => name.language.name === "fr")?.name;
+
+                  pokemonsData.push({
+                    nameFR: nameInFrench,
+                    name: pokemon.name,
+                    image: details.sprites.front_default,
+                  });
+
+                  setPokemons([...pokemonsData]);
+                });
+            })
+            .catch(error => {
+              console.error("Erreur lors de la récupération d'un Pokémon :", error);
+            });
+        });
       })
       .catch(error => {
         setError(error.message);
       });
-  }, []); 
+  }, []);
 
   return (
     <>
@@ -34,7 +64,7 @@ export default function GetPokemonList({ onPokemonClick }) {
         {pokemons.map((pokemon, index) => (
           <PokemonCard
             key={index}
-            name={pokemon.name}
+            name={pokemon.nameFR}
             image={pokemon.image}
             onClick={() => onPokemonClick(pokemon.name)}
           />
@@ -58,11 +88,11 @@ export function getPokemonStats(name) {
       return {
         name: data.name,
         stats: data.stats.map(stat => ({
-          name: stat.stat.name,
+          name: StatsTranslation[stat.stat.name] || stat.stat.name,
           value: stat.base_stat,
         })),
-        types: data.types.map(typeInfo => typeInfo.type.name),
-        abilities: data.abilities.map(abilityInfo => abilityInfo.ability.name),
+        types: data.types.map(typeInfo =>  TypesTranslation[typeInfo.type.name] || typeInfo.type.name),
+        abilities: data.abilities.map(abilityInfo => AbilitiesTranslation[abilityInfo.ability.name] || abilityInfo.ability.name ),
         image: data.sprites.front_default,
         imageShiny: data.sprites.front_shiny,
         id: data.id
@@ -73,3 +103,30 @@ export function getPokemonStats(name) {
       return null;
     });
 }
+
+export function getPokemonInformation(id) {
+  const url = `https://pokeapi.co/api/v2/pokemon-species/${id}/`
+
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Erreur de requête !");
+      }
+      return response.json();
+    })
+    .then(data => {
+      return {
+        name: data.names.find(name => name.language.name === "fr")?.name,
+        desc: data.flavor_text_entries.find(
+          entry => entry.language.name === "fr"
+        )?.flavor_text,
+      };
+    })
+    .catch(error => {
+      console.error("Erreur lors de la récupération des statistiques :", error);
+      return null;
+    });
+
+}
+
+console.log(getPokemonInformation(1))
---
+++ src/pages/api/translation.js
@@ -0,0 +1,27 @@
+export const StatsTranslation = {
+    hp: "PV",
+    attack: "Attaque",
+    defense: "Défense",
+    "special-attack": "Attaque Spéciale",
+    "special-defense": "Défense Spéciale",
+    speed: "Vitesse"
+};
+
+export const TypesTranslation = {
+    grass: "Plante",
+    water: "Eau",
+    fire: "Feu",
+    ghost: "Spectre",
+    ground: "Sol",
+    rock: "Roche",
+    bug: "Insecte",
+    dark: "Ténèbre",
+    electric: "Electrik",
+    fairy: "Fée",
+    fighting: "Combat",
+    flying: "Vol",
+    ice: "Glace",
+    psychic: "Psy",
+    Steel: "Acier",  
+}
+export const AbilitiesTranslation = {}
